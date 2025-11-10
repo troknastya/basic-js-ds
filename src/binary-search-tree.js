b052391e544caa8baaa6b9e314f7037ffb8df11c
nastassia trokailo <nstiatrok084@gmail.com>
2025-11-11 01:38:27 +0300

    add: try solve progect

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -5,43 +5,143 @@ const { NotImplementedError } = require('../lib/errors');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-class BinarySearchTree {
-  root() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
-
-  add(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
-
-  find(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
-
-  has(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
-
-  remove(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
-
-  min() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
-
-  max() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
 }
 
+class BinarySearchTree {
+  constructor() {
+    this._root = null;
+  }
+
+  root() {
+    return this._root;
+  }
+
+  add(data) {
+    const newNode = new Node(data);
+    
+    if (this._root === null) {
+      this._root = newNode;
+      return;
+    }
+
+    let current = this._root;
+    while (true) {
+      if (data < current.data) {
+        if (current.left === null) {
+          current.left = newNode;
+          return;
+        }
+        current = current.left;
+      } else if (data > current.data) {
+        if (current.right === null) {
+          current.right = newNode;
+          return;
+        }
+        current = current.right;
+      } else {
+        // Duplicate value - do nothing or handle as needed
+        return;
+      }
+    }
+  }
+
+  find(data) {
+    let current = this._root;
+    
+    while (current !== null) {
+      if (data === current.data) {
+        return current;
+      } else if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    
+    return null;
+  }
+
+  has(data) {
+    return this.find(data) !== null;
+  }
+
+  remove(data) {
+    this._root = this._removeNode(this._root, data);
+  }
+
+  _removeNode(node, data) {
+    if (node === null) {
+      return null;
+    }
+
+    if (data < node.data) {
+      node.left = this._removeNode(node.left, data);
+      return node;
+    } else if (data > node.data) {
+      node.right = this._removeNode(node.right, data);
+      return node;
+    } else {
+      // Node to be deleted found
+      
+      // Case 1: Node with no children
+      if (node.left === null && node.right === null) {
+        return null;
+      }
+      
+      // Case 2: Node with one child
+      if (node.left === null) {
+        return node.right;
+      }
+      if (node.right === null) {
+        return node.left;
+      }
+      
+      // Case 3: Node with two children
+      // Find the smallest node in the right subtree (in-order successor)
+      let minNode = this._findMinNode(node.right);
+      node.data = minNode.data;
+      node.right = this._removeNode(node.right, minNode.data);
+      return node;
+    }
+  }
+
+  _findMinNode(node) {
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node;
+  }
+
+  min() {
+    if (this._root === null) {
+      return null;
+    }
+    
+    let current = this._root;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
+  }
+
+  max() {
+    if (this._root === null) {
+      return null;
+    }
+    
+    let current = this._root;
+    while (current.right !== null) {
+      current = current.right;
+    }
+    return current.data;
+  }
+}
 module.exports = {
   BinarySearchTree
 };
